fb5956995252ca6233b1bfdde9964597a3e3659c
Ed Costello <edc.open@gmail.com>
2017-10-28 18:27:57 +1300

    Add ability to generate JWK keys from KMS master key

--- src/ocoen/docsenderlambda.py
+++ src/ocoen/docsenderlambda.py
@@ -1,14 +1,61 @@
+from jwcrypto import jwk
+from jwcrypto.common import base64url_encode
 from ocoen.docsender import DocSender
+from ulid import ulid
 
 import boto3
 import json
 import logging
 import os
+import threading
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 _docsender = None
+
+
+class TokenKeyProvider:
+
+    def __init__(self, kms_client, kms_key_id, keys_bucket, keys_bucket_prefix, keys_bucket_storage_class):
+        self._kms_client = kms_client
+        self._kms_key_id = kms_key_id
+        self._keys_bucket = keys_bucket
+        self._keys_bucket_prefix = keys_bucket_prefix
+        self._keys_bucket_storage_class = keys_bucket_storage_class
+        self._state = None
+        self._stateLock = threading.Lock()
+
+    def get_key(self):
+        with self._stateLock:
+            if self._state is None or self._state['remaining_uses'] <= 0:
+                self._state = self._generate_key()
+            self._state['remaining_uses'] -= 1
+            return self._state['key']
+
+    def _generate_key(self):
+        key_id = ulid()
+        response = self._kms_client.generate_data_key(
+            KeyId=self._kms_key_id,
+            KeySpec='AES_256',
+            EncryptionContext={
+                'key_id': key_id,
+                'key_role': 'docsender_tracking_token',
+            }
+        )
+        self._keys_bucket.put_object(
+            Key='{}docsender_tracking_token/{}'.format(
+                self._keys_bucket_prefix,
+                key_id
+            ),
+            Body=response['CiphertextBlob'],
+            ServerSideEncryption='AES256',
+            StorageClass=self._keys_bucket_storage_class,
+        )
+        return {
+            'remaining_uses': 2 ** 24,
+            'key': jwk.JWK(kty='oct', kid=key_id, k=base64url_encode(response['Plaintext'])),
+        }
 
 
 def load_docsender():
--- tests/test_docsenderlambda.py
+++ tests/test_docsenderlambda.py
@@ -1,6 +1,30 @@
+from jwcrypto.common import base64url_encode
+from ocoen.docsenderlambda import TokenKeyProvider
+from unittest.mock import create_autospec
+
+import json
 import ocoen.docsender
 import ocoen.docsenderlambda
-import json
+import os
+import pytest
+
+kms_key_id = 'test key'
+key_bytes = os.urandom(256 // 8)
+encrypted_key_bytes = os.urandom(64)
+keys_bucket_prefix = 'keys/'
+s3_storage_class = 'TestClass'
+
+
+@pytest.fixture
+def token_key_provider(session, s3_buckets):
+    kms = create_autospec(session.client('kms').__class__, instance=True)
+
+    kms.generate_data_key.return_value = {
+        'CiphertextBlob': encrypted_key_bytes,
+        'Plaintext': key_bytes,
+    }
+    keys_bucket = s3_buckets.Bucket('keys')
+    return TokenKeyProvider(kms, kms_key_id, keys_bucket, keys_bucket_prefix, s3_storage_class)
 
 
 def test_handle_event(mocker):
@@ -24,3 +48,70 @@ def test_handle_event(mocker):
     ocoen.docsenderlambda.handle_event(event, None)
 
     ocoen.docsenderlambda._docsender.send_email.assert_called_once_with(profile_key, result_key, sns_event)
+
+
+def test_token_key_provider_generates_key(token_key_provider):
+    key = token_key_provider.get_key()
+
+    assert key is not None
+    assert key.key_id is not None
+    assert key.is_symmetric
+
+
+def test_token_key_provider_returns_same_key(token_key_provider):
+    key1 = token_key_provider.get_key()
+    key2 = token_key_provider.get_key()
+
+    assert key1 == key2
+
+
+def test_token_key_provider_decrements_remaining_uses(token_key_provider):
+    token_key_provider.get_key()
+    remaining_uses_1 = token_key_provider._state['remaining_uses']
+    token_key_provider.get_key()
+    remaining_uses_2 = token_key_provider._state['remaining_uses']
+
+    assert remaining_uses_2 < remaining_uses_1
+
+
+def test_token_key_provider_gets_new_key_when_all_uses_used(token_key_provider):
+    token_key_provider.get_key()
+    token_key_provider._state['remaining_uses'] = 1
+    key1 = token_key_provider.get_key()
+    key2 = token_key_provider.get_key()
+    key3 = token_key_provider.get_key()
+
+    assert key1 != key2
+    assert key2 == key3
+
+
+def test_token_key_provider_generate_key_sets_remaining_uses(token_key_provider):
+    state = token_key_provider._generate_key()
+
+    assert state['remaining_uses'] == 2 ** 24
+
+
+def test_token_key_provider_generate_key_uses_kms(token_key_provider):
+    key = token_key_provider._generate_key()['key']
+
+    assert key._key['k'] == base64url_encode(key_bytes)
+    token_key_provider._kms_client.generate_data_key.assert_called_once_with(
+        KeyId=kms_key_id,
+        KeySpec='AES_256',
+        EncryptionContext={
+            'key_id': key.key_id,
+            'key_role': 'docsender_tracking_token',
+        },
+    )
+
+
+def test_token_key_provider_generate_key_saves_encrypted_key_to_s3(token_key_provider):
+    key = token_key_provider._generate_key()['key']
+
+    key_s3_key = '{}docsender_tracking_token/{}'.format(keys_bucket_prefix, key.key_id)
+    token_key_provider._keys_bucket.put_object.assert_called_once_with(
+        Key=key_s3_key,
+        Body=encrypted_key_bytes,
+        ServerSideEncryption='AES256',
+        StorageClass=s3_storage_class,
+    )
