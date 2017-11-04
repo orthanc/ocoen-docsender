e491d60bda11abf1d273971c6ccdd69888a988ee
Ed Costello <edc.open@gmail.com>
2017-11-04 15:46:26 +1300

    Fix pip_check to respect the constraints file

--- scripts/install-dependencies.py
+++ scripts/install-dependencies.py
@@ -1,8 +1,17 @@
 from setuptools.config import read_configuration
 
 import pip
+import sys
+
+if len(sys.argv) >= 2:
+    constraints_file = sys.argv[1]
+else:
+    constraints_file = None
 
 config_dict = read_configuration('setup.cfg')
 deps = config_dict['options']['install_requires']
 
-pip.main(['install', *deps])
+if constraints_file is None:
+    pip.main(['install', *deps])
+else:
+    pip.main(['install', '-c' + constraints_file, *deps])
